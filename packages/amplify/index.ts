d5ece3164dc99dd6c68c5b213e3cb6f6bb6a6124
Claudio Vasquez <nimbus@Mac.bbrouter>
2024-11-12 13:38:07 -0600

    amplify added successfully

---
+++ packages/amplify/index.ts
@@ -0,0 +1,7 @@
+import outputs from "./amplify_outputs.json";
+
+export * from "./amplify/backend";
+export * from "./amplify/auth/resource";
+export * from "./amplify/data/resource";
+
+export { outputs };

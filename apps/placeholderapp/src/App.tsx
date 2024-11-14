f10969aab1c2fe60b4fac3a63b5f0e388c3fa237
Claudio Vasquez <nimbus@Mac.bbrouter>
2024-11-14 14:56:22 -0600

    test

--- apps/placeholderapp/src/App.tsx
+++ apps/placeholderapp/src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
-//cambio pequeñito
 function App() {
-  return (
-    <>
-      <h1>Nothing to see here</h1>
-    </>
-  )
+	return (
+		<>
+			<h1>Nothing to see here</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
--- packages/amplify/index.ts
+++ packages/amplify/index.ts
@@ -1,7 +1,5 @@
-import outputs from "./amplify_outputs.json";
 
 export * from "./amplify/backend";
 export * from "./amplify/auth/resource";
 export * from "./amplify/data/resource";
 
-export { outputs };
